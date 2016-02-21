c5d49e302a24ff42d0a66e898dc595bf89279f3d
David Parry <drjparry@gmail.com>
2016-02-21 19:01:27 +0100

    added strike calculation

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -13,24 +13,49 @@ describe('BowlingGame', function() {
 
 
   it('Allows a user a first bowl', function() {
-    spyOn(bowlingGame, "_bowlCalculator").and.returnValue(7);
-    bowlingGame.firstBowl();
+    bowlingGame.firstBowl(7);
     expect(bowlingGame.roundScore).toEqual([7])
   });
 
 
   it('Reduces the pins for the next round by the number hit previously', function() {
-    spyOn(bowlingGame, "_bowlCalculator").and.returnValue(7);
-    bowlingGame.firstBowl();
+    bowlingGame.firstBowl(7);
     expect(bowlingGame.pins).toEqual(3)
 
   });
 
   it('ALlows a user a second turn to hit remaining pins', function() {
-    spyOn(bowlingGame, "_bowlCalculator").and.returnValue(3);
-    bowlingGame.firstBowl();
-    bowlingGame.secondBowl();
+    bowlingGame.firstBowl(3);
+    bowlingGame.secondBowl(3);
     expect(bowlingGame.roundScore).toEqual([3,3])
+  });
+
+  it('Adds a round after every turn', function() {
+    bowlingGame.firstBowl(3);
+    bowlingGame.secondBowl(3);
+    expect(bowlingGame.round).toEqual(1)
+  });
+
+  it('Calculates a total score', function() {
+    bowlingGame.firstBowl(3);
+    bowlingGame.secondBowl(3);
+    expect(bowlingGame.finalScore).toEqual(6)
+
+  });
+
+  it('Calculates a strike', function() {
+    bowlingGame.firstBowl(10);
+    bowlingGame.firstBowl(4);
+    bowlingGame.secondBowl(5);
+    expect(bowlingGame.finalScore).toEqual(28)
+  });
+
+  it('Calculates a spare', function() {
+    bowlingGame.firstBowl(7);
+    bowlingGame.secondBowl(3);
+    bowlingGame.firstBowl(4);
+    bowlingGame.secondBowl(5);
+    expect(bowlingGame.finalScore).toEqual(23)
   });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -3,25 +3,85 @@
 function BowlingGame () {
   this.score = [];
   this.roundScore = [];
-  this.round = 1;
+  this.round = 0;
   this.pins = 10;
+  this.finalScore = 0
 
-
-BowlingGame.prototype.firstBowl = function() {
-  var score = this._bowlCalculator()
-  this.pins -= score;
-  this.roundScore.push(score);
 };
 
-BowlingGame.prototype.secondBowl = function() {
-  var score = this._bowlCalculator()
-  this.roundScore.push(score);
-  this.pins = 10;
-  this.round ++;
+BowlingGame.prototype.firstBowl = function(bowl) {
+  if (bowl === 10) {
+    this.roundScore.push(bowl);
+    this._runRound();
+  } else {
+  this.pins -= bowl;
+  this.roundScore.push(bowl);
+  }
+};
+
+BowlingGame.prototype.secondBowl = function(bowl) {
+  this.roundScore.push(bowl);
+  this._runRound();
 };
 
 BowlingGame.prototype._bowlCalculator = function() {
-   Math.floor(Math.random() * this.pins) + 0;
+   return Math.floor(Math.random() * this.pins) + 0;
 };
 
+BowlingGame.prototype._runRound = function() {
+  this.score.push(this.roundScore);
+  this.pins = 10;
+  this.round++;
+  this.finalScore = this._getScore();
+
+
 };
+
+
+
+
+
+
+
+BowlingGame.prototype._getScore = function() {
+  var score = [].concat.apply([], this.score);
+  var scoreindex = 0;
+  var self = this;
+  var finalScore = 0;
+
+  function isStrike() {
+  return score[scoreindex] === 10;
+  };
+
+  function strikeBonus() {
+  return score[scoreindex + 1] + score[scoreindex + 2];
+  };
+
+  function isSpare(){
+    return score[scoreindex] + score[scoreindex]
+
+  }
+
+   function SumOfRound() {
+    return score[scoreindex] + score[scoreindex + 1];
+  };
+
+
+  for (var turn = 0; turn < (self.round); turn++){
+
+    if (isStrike()) {
+      finalScore += 10 + strikeBonus();
+      scoreindex++;
+    } else {
+      finalScore += SumOfRound();
+      scoreindex += 2;
+    }
+
+  }
+console.log(finalScore);
+return finalScore;
+
+
+};
+
+
